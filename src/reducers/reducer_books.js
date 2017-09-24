fb4b135ea54dce337916000c8733be72461ad8b1
moinulshaon <moinul.shaon@gmail.com>
2017-09-24 17:07:59 +0100

    saving commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Harry Potter' },
+        { title: 'JavaScript: The Good Parts' }, // eslint-disable-line
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' }
+    ];
+}
